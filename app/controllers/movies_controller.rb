1c6a186a1f6b46209d8549f504729f4138f869a4
dibiasemi <dibiase.111@gmail.com>
2017-11-02 16:52:25 -0500

    genre css and labeling

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,7 +2,6 @@ class MoviesController < ApplicationController
   def index
     # @movies = Movie.all
     @movies = Movie.order("created_at DESC").page params[:page]
-    # add a popularity ordering method
   end
 
 end
