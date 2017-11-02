ef1c1353ad97a23a6d3bbe3545cce4db28db8c7c
Kristin MacDonough <kristin.macdonough@gmail.com>
2017-11-01 20:36:47 -0500

    Update reviews_controller.rb

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,8 +5,13 @@ class ReviewsController < ApplicationController
 
   def create
     @movie = Movie.find(params[:movie_id])
-    @review = @movie.reviews.create(params[:review].permit(:content))
-
+    @review = @movie.reviews.create(review_params)
+    # change to save for error handling
     redirect_to movie_path(@movie)
+  end
+  
+  private
+  def review_params
+     params.require(:review).permit(:content)
   end
 end
