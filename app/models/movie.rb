eb85cadd476545b40b24c9eeb668fbbcaad59c6f
Melissa Kanhirun <mk7run@gmail.com>
2017-11-01 19:23:10 -0500

    Add associations of movies to genre

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,6 +2,7 @@ class Movie < ActiveRecord::Base
   has_many :reviews
   has_many :ratings
   has_many :labelings
+  has_many :genre, through: :labelings
 
   validates :title, :poster_url, presence: true
 end
